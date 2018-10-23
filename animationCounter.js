bdd3a835523606ead551b354181672a9eef80304
anucks <nuckles.a@gmail.com>
2018-10-23 16:53:26 -0700

    Update animationCounter.js

--- animationCounter.js
+++ animationCounter.js
@@ -42,3 +42,44 @@
 	};
 
 })(jQuery);
+
+// Add new
+
+(function($){
+
+	$.fn.countUp = function(options) {
+		return this.each(function() {
+			
+			<body>
+
+<p id="demo"></p>
+
+<script>
+// Set the date we're counting up from
+var LaunchDate = new Date("Oct 21, 2018 15:37:25").getTime();
+
+// Update the count up every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count up date
+    var distance = now - LaunchDate;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+</script>
+
+</body>
+		
+
+})(jQuery);
