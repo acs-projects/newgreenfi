1df6e428c3fbabed7535cb52d23e504e3bf8d82c
anucks <nuckles.a@gmail.com>
2018-10-24 08:35:00 -0700

    Update countUp.js

--- countUp.js
+++ countUp.js
@@ -1,18 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+p {
+  text-align: center;
+  font-size: 60px;
+  margin-top: 0px;
+}
+</style>
+</head>
 <body>
 
-<p id="demo"></p>
+<p id="clock"></p>
 
 <script>
-// Set the date we're counting up from
-var LaunchDate = new Date("Oct 21, 2018 15:37:25").getTime();
+// Set the date we're counting down to
+var LaunchDate = new Date("Oct 22, 2018 15:37:25").getTime();
 
-// Update the count up every 1 second
+// Update the count down every 1 second
 var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
     
-    // Find the distance between now and the count up date
+    // Find the distance between now and the count down date
     var distance = now - LaunchDate;
     
     // Time calculations for days, hours, minutes and seconds
@@ -20,12 +32,14 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var totalsec = Math.floor(distance / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    
+    document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s " +totalsec;
+}, 1000);
 </script>
 
 </body>
 </html>
+
